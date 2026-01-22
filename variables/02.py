d774181864b8d04422d30c2ed3a1e73c0ddb791a
ahmady016 <ahmady09@gmail.com>
2026-01-22 11:55:30 +0200

    build the second variables task

---
+++ variables/02.py
@@ -0,0 +1,17 @@
+# estimate the price cost for family passengers:
+# given the adult ticket price of $125
+# and the child ticket price is $70
+# and the number of adults is 2
+# and the number of children is 3
+
+adult_ticket_price = 125
+child_ticket_price = 70
+number_of_adults = 2
+number_of_children = 3
+
+total_cost = (adult_ticket_price * number_of_adults) + (child_ticket_price * number_of_children)
+
+print("cost for family passengers")
+print("---------------------------")
+print("total cost:", total_cost)
+print("---------------------------")
