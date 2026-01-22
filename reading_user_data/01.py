e8f06f8f1d1b2a4a393d33dbdf177bed21c70ca2
ahmady016 <ahmady09@gmail.com>
2026-01-22 13:04:06 +0200

    build the first input task

---
+++ reading_user_data/01.py
@@ -0,0 +1,13 @@
+# display user information, like name, city and country:
+# input the user's name
+# input the user's city and country
+
+user_name = input("Enter your name: ")
+user_city = input("Enter your city: ")
+user_country = input("Enter your country: ")
+
+print("------------------------------")
+print("Get and display User information:")
+print("------------------------------")
+print(user_name, "lives in", user_city, ",", user_country)
+print("------------------------------")
