d96e6c53821f47d0908d7cf1dd1913b2e510f987
ahmady016 <ahmady09@gmail.com>
2026-01-22 11:14:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# this is a comment
+print("--- print messages ---")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("-------------------------")
+
+print("--- arithmetic operations ---")
+print("funds to send:", 200*5 + 600/5 + 750-75)
+print("-------------------------")
+
+print("--- float division ---")
+print("30/9 =", 30/9)
+print("-------------------------")
+
+print("--- integer division ---")
+print("30//9 =", 30//9)
+print("-------------------------")
+
